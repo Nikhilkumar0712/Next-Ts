8a08ee05726b9b732f131ddc8ad0beb6e5324a33
UdifyTechnologies/ <itsudfiy@gmail.com>
2022-10-29 15:18:33 +0530

    V

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Box, Input, FormLabel, Heading, Textarea, Button, } from '@chakra-ui/react'
+import { useFileUpload } from 'use-file-upload'
+import './App.css'
+const App = () => {
+  const [file, selectFile] = useFileUpload()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Box
+        backgroundColor={'#3182ce'}>
+        <Box
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          mt={5}>
+          <Box
+            boxShadow={"0px 0px 10px #00000029;"}
+            backgroundColor={"#fff"}
+            width={"450px"}
+            padding={"20px"}>
+            <Box>
+              <Heading>Contact Us</Heading>
+
+              <Box >
+                <FormLabel>Your Name</FormLabel>
+                <Input placeholder='Your Name' />
+              </Box>
+              <Box mt={2}>
+                <FormLabel>Phone No</FormLabel>
+                <Input placeholder='Phone No' />
+              </Box>
+              <Box mt={2}>
+                <FormLabel>Email</FormLabel>
+                <Input placeholder='Email' />
+              </Box>
+              <Box mt={2}>
+                <FormLabel>Address</FormLabel>
+                <Textarea placeholder='Address' />
+              </Box>
+              <Box mt={2}>
+                <FormLabel>Message</FormLabel>
+                <Textarea placeholder='Message' />
+              </Box>
+              <Box mt={2}>
+                <Button colorScheme='blue' variant='solid'
+                  onClick={() => {
+                    selectFile()
+                  }}>Upload Image</Button>
+              </Box>
+              <Box mt={2}>
+                {
+                  file ? (
+                    <Box
+                      width={"150px"}
+                      mt={2}>
+                      <img src={file.source} alt='preview' />
+                    </Box>
+                  ) : null
+                }
+              </Box>
+              <Box textAlign={"center"} mt={2}>
+                <Button colorScheme='blue' variant='solid'>Submit</Button>
+              </Box>
+
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  )
 }
 
-export default App;
+export default App
